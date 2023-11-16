0c91045d936e5b4ea4c39cfefb17364916ca5325
Ryan Hamilton <rhamil4@gmu.edu>
2023-11-16 09:17:55 -0500

    moved bulk of code outside of mixin class file

--- src/main/java/ryan/furry/Furry.java
+++ src/main/java/ryan/furry/Furry.java
@@ -1,9 +1,96 @@
 package ryan.furry;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.render.VertexConsumer;
+import org.joml.Vector3f;
 
 public class Furry implements ModInitializer {
     @Override
     public void onInitialize() {
+    }
+
+    public static void dice(float[][] vrts, Vector3f transNorm, VertexConsumer vertexConsumer, int light, int overlay,
+                            float red, float green, float blue, float alpha) {
+
+        // TODO: animate scalar wrt time or ticks
+        Vector3f sa = new Vector3f(transNorm).mul(1 / 16f);
+        Vector3f sb = new Vector3f(transNorm).mul(1 / 12f);
+        Vector3f sc = new Vector3f(transNorm).mul(1 / 32f);
+        Vector3f sd = new Vector3f(transNorm).mul(1 / 48f);
+
+        float[] sums = new float[5];
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                sums[j] += vrts[i][j];
+            }
+        }
+
+        XYZUV vx0 = new XYZUV(vrts[0]);
+        XYZUV vx1 = new XYZUV(vrts[1]);
+        XYZUV vx2 = new XYZUV(vrts[2]);
+        XYZUV vx3 = new XYZUV(vrts[3]);
+
+        XYZUV mid = new XYZUV(sums).scale(1 / 4f);
+
+        XYZUV p12 = vx0.add(vx1).scale(1 / 2f);
+        XYZUV p14 = vx0.add(vx3).scale(1 / 2f);
+        XYZUV p32 = vx2.add(vx1).scale(1 / 2f);
+        XYZUV p34 = vx2.add(vx3).scale(1 / 2f);
+
+        // TODO: abstract for n rows and m columns of shells (or just do 2 minutes of research / rewatch video and make a real implementation)
+        Muncher HUNGY = (x, y, z, u, v) -> vertexConsumer
+                .vertex(x, y, z, red, green, blue, alpha, u, v, overlay, light, transNorm.x, transNorm.y, transNorm.z);
+
+        HUNGY.eat(vx0.add(sa));
+        HUNGY.eat(p12.add(sa));
+        HUNGY.eat(mid.add(sa));
+        HUNGY.eat(p14.add(sa));
+
+        HUNGY.eat(vx1.add(sb));
+        HUNGY.eat(p12.add(sb));
+        HUNGY.eat(mid.add(sb));
+        HUNGY.eat(p32.add(sb));
+
+        HUNGY.eat(vx2.add(sc));
+        HUNGY.eat(p34.add(sc));
+        HUNGY.eat(mid.add(sc));
+        HUNGY.eat(p32.add(sc));
+
+        HUNGY.eat(vx3.add(sd));
+        HUNGY.eat(p34.add(sd));
+        HUNGY.eat(mid.add(sd));
+        HUNGY.eat(p14.add(sd));
+    }
+
+    public static class XYZUV {
+        public float x, y, z, u, v;
+
+        public XYZUV(float x, float y, float z, float u, float v) {
+            this.x = x; this.y = y; this.z = z; this.u = u; this.v = v;
+        }
+
+        public XYZUV(float[] f) {
+            this.x = f[0]; this.y = f[1]; this.z = f[2]; this.u = f[3]; this.v = f[4];
+        }
+
+        public XYZUV scale(float f) {
+            return new XYZUV(x * f, y * f, z * f, u * f, v * f);
+        }
+
+        public XYZUV add(Vector3f vec) {
+            return new XYZUV(x + vec.x, y + vec.y, z + vec.z, u, v);
+        }
+
+        public XYZUV add(XYZUV vec) {
+            return new XYZUV(x + vec.x, y + vec.y, z + vec.z, u + vec.u, v + vec.v);
+        }
+    }
+
+    public interface Muncher {
+        void eat(float x, float y, float z, float u, float v);
+
+        default void eat(XYZUV vec) {
+            eat(vec.x, vec.y, vec.z, vec.u, vec.v);
+        }
     }
 }
--- src/main/java/ryan/furry/mixin/CuboidMixin.java
+++ src/main/java/ryan/furry/mixin/CuboidMixin.java
@@ -8,71 +8,78 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import ryan.furry.Furry;
 
 @Mixin({ModelPart.Cuboid.class})
-public abstract class CuboidMixin {
+public class CuboidMixin {
     @Shadow @Final private ModelPart.Quad[] sides;
 
     /**
      * @author ryan
-     * @reason as a standalone mod, this will be a major optimization whilst having no effect on any human to ever live.
+     * @reason as a random mod, this will be a major optimization whilst having no effect on any human to ever live.
      */
     @Overwrite
-    public void renderCuboid(MatrixStack.Entry entry, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
+    public void renderCuboid(MatrixStack.Entry entry, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
         Matrix4f position = entry.getPositionMatrix();
         Matrix3f normal = entry.getNormalMatrix();
         for (ModelPart.Quad quad : this.sides) {
             Vector3f transNorm = normal.transform(new Vector3f(quad.direction));
-            float[][] verticies = new float[4][5];
+            float[][] vrts = new float[4][5];
             for (int index = 0; index < quad.vertices.length; ++index) {
                 ModelPart.Vertex vertex = quad.vertices[index];
                 float i = vertex.pos.x() / 16.0f;
                 float j = vertex.pos.y() / 16.0f;
                 float k = vertex.pos.z() / 16.0f;
                 Vector4f rotVert = position.transform(new Vector4f(i, j, k, 1.0f));
-                vertices.vertex(rotVert.x(), rotVert.y(), rotVert.z(), red, green, blue, alpha, vertex.u, vertex.v, overlay, light, transNorm.x, transNorm.y, transNorm.z);
-                verticies[index] = new float[]{rotVert.x, rotVert.y, rotVert.z, vertex.u, vertex.v};
+                vertexConsumer.vertex(rotVert.x(), rotVert.y(), rotVert.z(), red, green, blue, alpha, vertex.u, vertex.v, overlay, light, transNorm.x, transNorm.y, transNorm.z);
+                vrts[index] = new float[]{rotVert.x, rotVert.y, rotVert.z, vertex.u, vertex.v};
             }
 
-            // TODO: animate scalar wrt time or ticks
-            Vector3f sa = new Vector3f(transNorm).mul(1 / 16f);
-            Vector3f sb = new Vector3f(transNorm).mul(1 / 12f);
-            Vector3f sc = new Vector3f(transNorm).mul(1 / 32f);
-            Vector3f sd = new Vector3f(transNorm).mul(1 / 48f);
+            Furry.dice(vrts, transNorm, vertexConsumer, light, overlay, red, green, blue, alpha);
 
-            // TODO: abstract for n rows and m columns of shells (or just do 2 minutes of research / rewatch video and make a real implementation)
-            float[] p1 = verticies[0], p2 = verticies[1], p3 = verticies[2], p4 = verticies[3];
-            Vector3f mid = new Vector3f((p1[0] + p2[0] + p3[0] + p4[0]) / 4, (p1[1] + p2[1] + p3[1] + p4[1]) / 4, (p1[2] + p2[2] + p3[2] + p4[2]) / 4);
-            Vector3f b12 = new Vector3f((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2, (p1[2] + p2[2]) / 2);
-            Vector3f b14 = new Vector3f((p1[0] + p4[0]) / 2, (p1[1] + p4[1]) / 2, (p1[2] + p4[2]) / 2);
-            Vector3f b32 = new Vector3f((p3[0] + p2[0]) / 2, (p3[1] + p2[1]) / 2, (p3[2] + p2[2]) / 2);
-            Vector3f b34 = new Vector3f((p4[0] + p3[0]) / 2, (p4[1] + p3[1]) / 2, (p4[2] + p3[2]) / 2);
+            /*// max number of layers
+            int max = 16;
 
-            Vector2f uvm = new Vector2f((p1[3] + p2[3] + p3[3] + p4[3]) / 4, (p1[4] + p2[4] + p3[4] + p4[4]) / 4);
-            Vector2f t12 = new Vector2f((p1[3] + p2[3]) / 2, (p1[4] + p2[4]) / 2);
-            Vector2f t14 = new Vector2f((p1[3] + p4[3]) / 2, (p1[4] + p4[4]) / 2);
-            Vector2f t32 = new Vector2f((p3[3] + p2[3]) / 2, (p3[4] + p2[4]) / 2);
-            Vector2f t34 = new Vector2f((p4[3] + p3[3]) / 2, (p4[4] + p3[4]) / 2);
+            // space between layers
+            float d = 1f / (max * 8);
 
-            vertices.vertex(p1[0] + sa.x, p1[1] + sa.y, p1[2] + sa.z, red, green, blue, alpha, p1[3], p1[4], overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(b12.x + sa.x, b12.y + sa.y, b12.z + sa.z, red, green, blue, alpha, t12.x, t12.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(mid.x + sa.x, mid.y + sa.y, mid.z + sa.z, red, green, blue, alpha, uvm.x, uvm.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(b14.x + sa.x, b14.y + sa.y, b14.z + sa.z, red, green, blue, alpha, t14.x, t14.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
+            int rows = 2;
+            int cols = 2;
 
-            vertices.vertex(p2[0] + sb.x, p2[1] + sb.y, p2[2] + sb.z, red, green, blue, alpha, p2[3], p2[4], overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(b12.x + sb.x, b12.y + sb.y, b12.z + sb.z, red, green, blue, alpha, t12.x, t12.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(mid.x + sb.x, mid.y + sb.y, mid.z + sb.z, red, green, blue, alpha, uvm.x, uvm.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(b32.x + sb.x, b32.y + sb.y, b32.z + sb.z, red, green, blue, alpha, t32.x, t32.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
+            int total = rows * cols;
 
-            vertices.vertex(p3[0] + sc.x, p3[1] + sc.y, p3[2] + sc.z, red, green, blue, alpha, p3[3], p3[4], overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(b34.x + sc.x, b34.y + sc.y, b34.z + sc.z, red, green, blue, alpha, t34.x, t34.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(mid.x + sc.x, mid.y + sc.y, mid.z + sc.z, red, green, blue, alpha, uvm.x, uvm.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(b32.x + sc.x, b32.y + sc.y, b32.z + sc.z, red, green, blue, alpha, t32.x, t32.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
+            float dx = sums[0] / total;
+            float dy = sums[1] / total;
+            float dz = sums[2] / total;
+            float du = sums[3] / total;
+            float dv = sums[4] / total;
 
-            vertices.vertex(p4[0] + sd.x, p4[1] + sd.y, p4[2] + sd.z, red, green, blue, alpha, p4[3], p4[4], overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(b34.x + sd.x, b34.y + sd.y, b34.z + sd.z, red, green, blue, alpha, t34.x, t34.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(mid.x + sd.x, mid.y + sd.y, mid.z + sd.z, red, green, blue, alpha, uvm.x, uvm.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
-            vertices.vertex(b14.x + sd.x, b14.y + sd.y, b14.z + sd.z, red, green, blue, alpha, t14.x, t14.y, overlay, light, transNorm.x, transNorm.y, transNorm.z);
+            XYZUV p12 = new XYZUV(vrts[0][0] + vrts[1][0], vrts[0][1] + vrts[1][1], vrts[0][2] + vrts[1][2], vrts[0][3] + vrts[1][3], vrts[0][4] + vrts[1][4]);
+            XYZUV p14 = new XYZUV(vrts[0][0] + vrts[3][0], vrts[0][1] + vrts[3][1], vrts[0][2] + vrts[3][2], vrts[0][3] + vrts[3][3], vrts[0][4] + vrts[3][4]);
+            XYZUV p32 = new XYZUV(vrts[2][0] + vrts[1][0], vrts[2][1] + vrts[1][1], vrts[2][2] + vrts[1][2], vrts[2][3] + vrts[1][3], vrts[2][4] + vrts[1][4]);
+            XYZUV p34 = new XYZUV(vrts[2][0] + vrts[3][0], vrts[2][1] + vrts[3][1], vrts[2][2] + vrts[3][2], vrts[2][3] + vrts[3][3], vrts[2][4] + vrts[3][4]);
+
+            float delta = 1f / total;
+            //p12.scale(delta);
+            //p14.scale(delta);
+            //p32.scale(delta);
+            //p34.scale(delta);
+
+            for (int row = 0; row < rows; ++row) {
+                for (int col = 0; col < cols; ++col) {
+                    int index = row * cols + col;
+                    double progress = (float) (index + 1) / total;
+                    int layers = (int) (max * Math.exp(-Math.pow((double) 2 * index / total - 1, 2)));
+
+                    for (int layer = 0; layer < layers; ++layer) {
+                        float h = d * layer;
+                        Vector3f s = new Vector3f(transNorm).mul(h);
+                        for (int c = 1; c < 5; ++c) {
+                            //vertexConsumer.vertex(x * c + s.x, y * c + s.y, z * c + s.z, red, green, blue, alpha, u * c, v * c, overlay, light, transNorm.x, transNorm.y, transNorm.z);
+                        }
+                    }
+                }
+            }*/
         }
     }
 }
